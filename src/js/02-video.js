a4eb86e04d592601fdafcefdbddf50166b265445
Artur <terrenss666@gmail.com>
2023-07-14 19:30:44 +0200

    task 3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+    const STORAGE_KEY = "videoplayer-current-time";
+    
+player.on('timeupdate', throttle(saveTime, 1000));
 
+function saveTime(data) {
+        const time = data.seconds;
+        localStorage.setItem(STORAGE_KEY, time)
+
+}
+
+const currentTime = localStorage.getItem(STORAGE_KEY)
+
+if (currentTime) {
+player.setCurrentTime(currentTime);
+}
